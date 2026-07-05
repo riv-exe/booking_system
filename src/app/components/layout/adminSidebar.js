7264e312d18f6f68b3b3fec5687fbd8083b3c925
Paulo <buta.pauloreeve082527@gmail.com>
2026-07-05 21:35:45 +0800

    temp: remove pos on admin sidebar

--- src/app/components/layout/adminSidebar.js
+++ src/app/components/layout/adminSidebar.js
@@ -10,7 +10,7 @@ export default function AdminSidebar({ page, setPage }) {
                 <button className={`px-3 py-2 cursor-pointer w-full text-start rounded-lg ${page === 1 && "font-bold bg-gray-50/20"}`} onClick={() => setPage(1)}>Dashboard</button>
                 <button className={`px-3 py-2 cursor-pointer w-full text-start rounded-lg ${page === 2 && "font-bold bg-gray-50/20"}`} onClick={() => setPage(2)}>Bookings</button>
                 <button className={`px-3 py-2 cursor-pointer w-full text-start rounded-lg ${page === 3 && "font-bold bg-gray-50/20"}`} onClick={() => setPage(3)}>Court Schedule</button>
-                <button className={`px-3 py-2 cursor-pointer w-full text-start rounded-lg ${page === 4 && "font-bold bg-gray-50/20"}`} onClick={() => setPage(4)}>POS (Point of Sale)</button>
+                {/* <button className={`px-3 py-2 cursor-pointer w-full text-start rounded-lg ${page === 4 && "font-bold bg-gray-50/20"}`} onClick={() => setPage(4)}>POS (Point of Sale)</button> */}
             </div>
         </div>
     )
